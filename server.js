66b334587b0591cac74614339f3161cda574bf80
jx0906 <sandy_hong@live.com>
2023-12-30 12:15:02 +0800

    install CORS to facilitate linking of frontend and backend

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 //import controller
 var logger = require('morgan');
 
+// import CORS
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,12 +21,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//mount middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
+// mount routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
