c5dc87fdb453f438c85dd956a583ff0cb5d598d5
jx0906 <sandy_hong@live.com>
2024-01-11 22:58:05 +0800

    set up util and middlewares folder. also added the to-dos inside for future ref

---
+++ middlewares/security.js
@@ -0,0 +1,12 @@
+/* to do: create functions for
+checkJWT,
+    checkLogin,
+    checkPermission */
+
+// const utilSecurity = require("../util/security")
+
+// module.exports = {
+//     checkJWT,
+//     checkLogin,
+//     checkPermission
+// };
