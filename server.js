4cfd645c575e184a6702373481c4ec900c364b2c
cedricyong00 <cedricyong2000@gmail.com>
2024-01-21 19:39:58 +0800

    Initial commit for user

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var indexRouter = require("./routes/index");
 var userRouter = require("./routes/user");
 var restaurantRouter = require("./routes/restaurant");
 var bookingRouter = require("./routes/booking");
+var accountRouter = require("./routes/account")
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/restaurant", restaurantRouter);
 app.use("/booking", bookingRouter);
+app.use("/account"), accountRouter
 
 // catch 404 and forward to error handler
 app.use(function (req, res) {
